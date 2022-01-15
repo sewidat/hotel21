e0339d0827392338a81218c1f52626a6fdfb638d
sam <sewidat@gmail.com>
2022-01-15 19:46:37 +0000

    start with barakat allah

--- app/src/main/java/com/example/hotel21/controller/RoomView.java
+++ app/src/main/java/com/example/hotel21/controller/RoomView.java
@@ -3,13 +3,15 @@ package com.example.hotel21.controller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 import com.example.hotel21.R;
 
 public class RoomView extends AppCompatActivity {
-
+    GridView gridView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_view);
     }
